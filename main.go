0ec8cae0a0ebd4d3e0e2001618c6f6b737597c15
Andres Erbsen <andreser@mit.edu>
2016-07-24 19:35:46 -0400

    letsencrypt reverse proxy; TODO: check list membership

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    	"log"
+    	"net/url"
+    	"net/http"
+	"net/http/httputil"
+    	"rsc.io/letsencrypt"
+)
+
+func main() {
+	dst, err := url.Parse("http://127.0.0.1:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+    	http.Handle("/", httputil.NewSingleHostReverseProxy(dst))
+
+    	var m letsencrypt.Manager
+    	if err := m.CacheFile("letsencrypt.cache"); err != nil {
+    		log.Fatal(err)
+    	}
+    	log.Fatal(m.Serve())
+    }
