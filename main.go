de76d631324382b73ab909d29458266821fba6ed
Andres Erbsen <andreser@mit.edu>
2016-07-24 19:42:21 -0400

    make our own director /NT

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 	"net/http/httputil"
     	"rsc.io/letsencrypt"
 )
-
 func main() {
 	dst, err := url.Parse("http://127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    	http.Handle("/", httputil.NewSingleHostReverseProxy(dst))
+    	http.Handle("/", &httputil.ReverseProxy{Director: httputil.NewSingleHostReverseProxy(dst).Director})
 
     	var m letsencrypt.Manager
     	if err := m.CacheFile("letsencrypt.cache"); err != nil {
     		log.Fatal(err)
     	}
     	log.Fatal(m.Serve())
-    }
+}
